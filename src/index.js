af7902c23f88abf0bcca5fc365193ca05bbd8804
Oluwafemi <femikola23@gmail.com>
2021-12-08 00:40:18 +0400

    update index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import BookStore from './components/BookStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <BookStore />
+      <Provider store={store}>
+        <BookStore />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
