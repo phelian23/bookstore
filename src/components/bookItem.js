2ba191659e74c7fe34526aac7523ba3ecc726c94
Oluwafemi <femikola23@gmail.com>
2021-12-07 05:46:29 +0400

    Add book list

---
+++ src/components/bookItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BookItem = () => (
+  <li>
+    <button type="button">Remove</button>
+  </li>
+);
+
+export default BookItem;
---
+++ src/components/bookList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookItem from './bookItem';
+
+const BookList = () => (
+  <ul>
+    <BookItem />
+  </ul>
+);
+
+export default BookList;
