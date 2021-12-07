81ba56f46dab199a1a9943a13e1fac87b05b5299
Oluwafemi <femikola23@gmail.com>
2021-12-07 05:46:06 +0400

    Add Bookstore

---
+++ src/components/BookStore.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './navbar';
+import Books from '../redux/books/books';
+import Categories from '../redux/categories/categories';
+
+const BookStore = () => (
+  <div>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </div>
+);
+
+export default BookStore;
