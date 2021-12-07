570fa72bf8ae6e87f9baf4764789050b760cfb08
Oluwafemi <femikola23@gmail.com>
2021-12-07 06:20:35 +0400

    Add styles file

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './navbar';
 import Books from '../redux/books/books';
 import Categories from '../redux/categories/categories';
+import './App.css'
 
 const BookStore = () => (
   <div>
