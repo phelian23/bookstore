e8c7bf23f0167616594fe8dd5f00e22d8372ef96
Oluwafemi <femikola23@gmail.com>
2021-12-09 15:32:42 +0400

    Refactor bookpage to use API

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookList from './bookList';
 import AddNew from './addNew';
+import { getAllBooks } from '../redux/books/books';
 
 const Books = () => {
+  const [itemid, setItemid] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllBooks());
+  }, [itemid, dispatch]);
+
   const books = useSelector((state) => state.booksReducer);
 
   return (
     <div className="container">
       <div className="inner">
         <BookList books={books} />
-        <AddNew />
+        <AddNew setItemid={setItemid} />
       </div>
     </div>
   );
