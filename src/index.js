b9481b2acc4a9be96c3f7f802e5b63a9dfdfeea5
Oluwafemi <femikola23@gmail.com>
2021-12-10 01:42:17 +0400

    Add styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import BookStore from './components/BookStore';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
