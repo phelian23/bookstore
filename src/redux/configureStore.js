1595c4743326effc301680e97d5168b8a1171b62
Oluwafemi <femikola23@gmail.com>
2021-12-09 15:33:10 +0400

    Configure Middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {
+  createStore, combineReducers, compose, applyMiddleware,
+} from 'redux';
 import logger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
@@ -7,9 +10,11 @@ const reducer = combineReducers({
   // additional reducers could be added here
 });
 
+const composed = compose(applyMiddleware(thunkMiddleware, logger));
+
 const store = createStore(
   reducer,
-  applyMiddleware(logger),
+  composed,
 );
 
 export default store;
