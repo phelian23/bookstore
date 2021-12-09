ac95a13843769aa5bbcaf444a8e44d49ae8131e3
Oluwafemi <femikola23@gmail.com>
2021-12-10 01:45:05 +0400

    Add styles to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
+import styles from '../styles/navbar.module.css';
 
 const Navbar = () => (
-  <header>
-    <h1>Bookstore CMS</h1>
-    <ul>
-      <li>
-        <Link to="/">Books</Link>
+  <header className={styles.navbar}>
+    <h1 className={styles.bookStore}>Bookstore CMS</h1>
+    <ul className={styles.list}>
+      <li className={styles.listItem1}>
+        <Link to="/" className={styles.listLink}>BOOKS</Link>
       </li>
-      <li>
-        <Link to="/categories">Categories</Link>
+      <li className={styles.listItem2}>
+        <Link to="/categories" className={styles.listLink}>CATEGORIES</Link>
       </li>
     </ul>
+    <div className={styles.userIconCont}>
+      <ImUser className={styles.userIcon} />
+    </div>
   </header>
 );
 
