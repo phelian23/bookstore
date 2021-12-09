d2b1c1b1baa09b4f3e3522056bca6a422524003f
Oluwafemi <femikola23@gmail.com>
2021-12-10 01:44:29 +0400

    Add styles to bookstore

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -3,15 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './navbar';
 import Books from './BookPage';
 import Categories from './CategoriesPage';
-import './App.css';
+import styles from '../styles/BookStore.module.css';
 
 const BookStore = () => (
-  <div>
-    <Navbar />
-    <Routes>
-      <Route exact path="/" element={<Books />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
+  <div className={styles.bookStore}>
+    <div className={styles.bookStoreCont}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
   </div>
 );
 
