191a37e6b07bded5feb721519469c0341085743f
Oluwafemi <femikola23@gmail.com>
2021-12-10 13:12:55 +0400

    Update styles

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 import styles from '../styles/bookItem.module.css';
+import Capturepie from '../images/Capturepie.PNG';
 
 const BookItem = ({ book }) => {
   const dispatch = useDispatch();
@@ -14,12 +15,25 @@ const BookItem = ({ book }) => {
 
   return (
     <li className={styles.listCont}>
-      <div>
+      <div className={styles.box1}>
         <h5 className={styles.category}>{category}</h5>
         <h3 className={styles.title}>{title}</h3>
+        <p className={styles.author}>Wole Shoyinka</p>
         <button type="button" className={styles.listBtn}>Comment</button>
         <button type="button" className={styles.listBtn} onClick={() => dispatch(removeBook(id))}>Remove</button>
         <button type="button" className={styles.listBtn}>Edit</button>
+      </div>
+      <div className={styles.box2}>
+        <img src={Capturepie} alt="pie" />
+        <div>
+          <p className={styles.percentComplete}>64%</p>
+          <p className={styles.completed}>Completed</p>
+        </div>
+      </div>
+      <div className={styles.box3}>
+        <p className={styles.currentChapter}>CURRENT CHAPTER</p>
+        <p className={styles.currentLesson}>Chapter 17</p>
+        <button className={styles.rectangle} type="button">UPDATE PROGRESS</button>
       </div>
     </li>
   );
